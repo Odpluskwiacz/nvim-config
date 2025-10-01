a3a4d4e73a6522ed6197fb4b92484efc176365ae
Odpluskwiacz <michal.bralski@tutanota.com>
2025-10-01 16:05:35 +0200

    Grzebię w keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,3 +3,32 @@
 -- THEME / MOTYW
 vim.cmd.colorscheme("unokai")
 
+-- Transparency
+--vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+--vim.api.nvim_set_hl(0, "NormalNC", {bg = "none"})
+--vim.api.nvim_set_hl(0, "EndOfBuffer", {bg = "none"})
+
+
+
+-- Basic Setings
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
+
+-- Indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+
+-- Visual settings
+vim.opt.termguicolors = true
+
+
+
