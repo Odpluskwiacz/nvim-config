aa90dbc0e3d75fa20322661b928ec8f54c1877c2
Odpluskwiacz <michal.bralski@tutanota.com>
2025-11-24 01:26:24 +0100

    Piękne Błędu z fajnymi kropkami i lepszym tłumaczeniem zamiast samo {E} albo {W}

--- init.lua
+++ init.lua
@@ -23,3 +23,20 @@ vim.api.nvim_create_autocmd("FileType", {
     end
   end,
 })
+
+-- Pokazuje pełny opis błędu
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●",
+    spacing = 1,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+
+
+
+
